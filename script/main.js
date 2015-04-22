e264b384aeec3fdc8ed3b1024b3947cd656d8e72
Dan Foad <danielfoad@gmail.com>
2015-04-22 13:26:59 +0100

    Updated with newest version

--- script/main.js
+++ script/main.js
@@ -41,3 +41,38 @@ $(".button__contact").click(function() {
         scrollTop: $("#contact").offset().top
     }, "fast");
 });
+
+// Contact Form validation
+$("#contact__name").on("keydown cut paste input", function() {
+    if ($("#contact__name").val().length > 0) {
+        $("#contact__name--valid").show().html("<i class='fa fa-check'></i>").addClass("valid");
+        if ($("#contact__name--valid").hasClass("invalid")) $("#contact__name--valid").removeClass("invalid"); 
+    } else {
+        if ($("#contact__name--valid").hasClass("valid")) $("#contact__name--valid").html("<i class='fa fa-times'></i>").removeClass("valid").addClass("invalid");
+    }
+});
+
+$("#contact__email").on("keydown cut paste input", function() {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test($("#contact__email").val())) {
+        $("#contact__email--valid").show().html("<i class='fa fa-check'></i>").addClass("valid");
+        if ($("#contact__email--valid").hasClass("invalid")) $("#contact__email--valid").removeClass("invalid"); 
+    } else {
+        if ($("#contact__email--valid").hasClass("valid")) $("#contact__email--valid").html("<i class='fa fa-times'></i>").removeClass("valid").addClass("invalid");
+    }
+});
+
+$("#contact__form").on("submit", function(e) {
+    e.preventDefault();
+    $.ajax({
+        url: $(this).attr("action"),
+        type: 'POST',
+        data: $(this).serialize(),
+        beforeSend: function() {
+            $("#contact__response").show().html("Sending message...");
+        },
+        success: function(data) {
+            $("#contact__response").html(data);
+        }
+    });
+});
--- script/page.js
+++ script/page.js
@@ -34,3 +34,22 @@ $("#top").click(function() {
         scrollTop: 0
     }, "fast");
 });
+
+// Code Highlighting
+hljs.initHighlightingOnLoad();
+        $(document).ready(function() {
+            lineCode();
+        });
+
+        function lineCode() {
+            var pre = document.getElementsByTagName('pre'),
+                pl = pre.length;
+            for (var i = 0; i < pl; i++) {
+                pre[i].innerHTML = '<span class="line-number"></span>' + pre[i].innerHTML + '<span class="cl"></span>';
+                var num = pre[i].innerHTML.split(/\n|<br>/).length;
+                for (var j = 0; j < num; j++) {
+                    var line_num = pre[i].getElementsByTagName('span')[0];
+                    line_num.innerHTML += '<span>' + (j + 1) + '</span>';
+                }
+            }
+        }
