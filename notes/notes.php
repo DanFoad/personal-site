5a1ebf6e7b71e29fa5df14a222f463e35d1cd14f
Dan Foad <danielfoad@gmail.com>
2017-12-16 22:15:34 +0000

    notes api

--- notes/notes.php
+++ notes/notes.php
@@ -1,2 +1,18 @@
 <?php
-    echo "hello world";
+
+$request = $_SERVER['REQUEST_URI'];
+$request = explode('/', $request);
+
+$method; $id;
+if (count($request > 2)) {
+    if (strcasecmp($request[2], 'get')) {
+        $method = 'GET';
+    }
+}
+if (count($request > 3)) {
+    $id = $request[3];
+}
+
+header('Content-Type: application/json');
+
+if (isset($id)) echo json_encode($id);
