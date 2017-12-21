0d63bb3bc0dc280d745cb297a7fd3ba107d1f729
Dan Foad <danielfoad@gmail.com>
2017-12-21 18:23:11 +0000

    Added update/create methods for notes

--- notes/notes.php
+++ notes/notes.php
@@ -14,12 +14,20 @@ if (!isSecure()) {
 require_once 'config.php';
 require_once 'jwt_helper.php';
 
-$headers = getallheaders();
-$method = $headers['Method'];
+if (!isset($_POST['method'])) {
+    header("HTTP/1.1 400 Bad Request");
+    echo "<h1>HTTP 400 Bad Request</h1><br>Invalid request, try again with different parameters";
+    die();
+}
+
+$method = $_POST['method'];
 
 switch($method) {
     case 'start_session': startSession($db); break;
     case 'get_notes': getNotes($db); break;
+    case 'get_tags': getTags($db); break;
+    case 'update_note': updateNote($db); break;
+    case 'create_note': createNote($db); break;
     default: 
         header("HTTP/1.1 400 Bad Request");
         echo "<h1>HTTP 400 Bad Request</h1><br>Invalid request, try again with different parameters";
@@ -67,7 +75,7 @@ function startSession($db) {
     echo JWT::encode($token, SERVER_KEY);
 }
 
-function getNotes($db) {
+function getID() {
     if (!isset($_POST['token'])) {
         header("HTTP/1.1 401 Authorisation Required");
         die();
@@ -75,6 +83,12 @@ function getNotes($db) {
 
     $token = JWT::decode($_POST['token'], SERVER_KEY);
     $id = $token->id;
+
+    return $id;
+}
+
+function getNotes($db) {
+    $id = getID();
 
     $sql = 'SELECT id, name, date, text
             FROM notes
@@ -84,4 +98,71 @@ function getNotes($db) {
     $results = $sth->fetchAll();
 
     echo json_encode($results);
+}
+
+function getTags($db) {
+    $id = getID();
+
+    $sql = 'SELECT id, name
+            FROM tag
+            WHERE user_id = :user_id';
+    $sth = $db->prepare($sql);
+    $sth->execute(array(':user_id' => $id));
+    $results = $sth->fetchAll();
+
+    echo json_encode($results);
+}
+
+function updateNote($db) {
+    $id = getID();
+
+    if (!isset($_POST['note'])) {
+        header("HTTP/1.1 400 Bad Request");
+        echo "<h1>HTTP 400 Bad Request</h1><br>Invalid request, try again with different parameters";
+        die();
+    }
+
+    $note = json_decode($_POST['note'], true);
+    
+    $sql = 'UPDATE notes
+            SET name = :name, tag = :tag, date = :date, text = :text
+            WHERE id = :id';
+    $sth = $db->prepare($sql);
+    $sth->execute(array(':name' => $note['name'], ':tag' => $note['tag'], ':date' => $note['date'], ':text' => $note['text'], ':id' => $note['id']));
+
+    $response = array();
+    if ($sth->rowCount() > 0) {
+        $response['success'] = true;
+        echo json_encode($response);
+    } else {
+        $response['success'] = false;
+        echo json_encode($response);
+    }
+}
+
+function createNote($db) {
+    $id = getID();
+
+    if (!isset($_POST['note'])) {
+        header("HTTP/1.1 400 Bad Request");
+        echo "<h1>HTTP 400 Bad Request</h1><br>Invalid request, try again with different parameters";
+        die();
+    }
+
+    $note = json_decode($_POST['note'], true);
+    
+    $sql = 'INSERT INTO notes (name, user_id, tag, date, text)
+            VALUES (:name, :user_id, :tag, :date, :text)';
+    $sth = $db->prepare($sql);
+    $sth->execute(array(':name' => $note['name'], ':user_id' => $id, ':tag' => $note['tag'], ':date' => $note['date'], ':text' => $note['text']));
+
+    $response = array();
+    if ($sth->rowCount() > 0) {
+        $response['success'] = true;
+        $response['id'] = $db->lastInsertId();
+        echo json_encode($response);
+    } else {
+        $response['success'] = false;
+        echo json_encode($response);
+    }
 }
